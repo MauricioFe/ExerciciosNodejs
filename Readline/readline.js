b5f139ec3e610fe10268d120cb831e67561e1dc2
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-07-30 20:06:02 -0300

    Utilizando o ReadLine

---
+++ Readline/readline.js
@@ -0,0 +1,18 @@
+import readline from "readline";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("digite um número: ", numero => {
+    const multiplos = []
+    for (let i = 0; i < parseInt(numero); i++) {
+        if ((i % 3 === 0) || (i % 5 === 0)) {
+            multiplos.push(i);
+        }
+    }
+
+    console.log(multiplos);
+    rl.close();
+})
